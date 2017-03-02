4a93a0a850d6db09d792c56bd3144377dd9cfe6e
Mathias Francke <mathias.francke@initos.com>
2017-03-02 17:11:09 +0100

    [FIX] Fixed Add to cart after scanning unexpected products.

--- stock_picking_barcode/models.py
+++ stock_picking_barcode/models.py
@@ -189,9 +189,9 @@ class StockPicking(models.Model):
     def action_done_from_ui(self, picking_id):
         """ called when button 'done' is pushed in the barcode scanner UI """
         # write qty_done into field product_qty for every package_operation before doing the transfer
-        for operation in self.browse(picking_id).pack_operation_ids:
+        for operation in self.pack_operation_ids:
             operation.with_context(no_recompute=True).write({'product_qty': operation.qty_done})
-        self.do_transfer([picking_id])
+        self.do_transfer()
         # return id of next picking to work on
         return self.get_next_picking_for_ui()
 
